2524c325d9379297687ee06dace9c38455759d1c
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-07-11 19:58:07 +0530

    made little about feedposts

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,15 @@
 import { Box, Container, Flex } from '@chakra-ui/react'
-import React from 'react'
+import FeedPosts from "../../components/FeedPosts/FeedPosts"
 
 function HomePage() {
   return (
     <Container maxW={"container.lg"}>
-      <Flex>
-        <Box>
-          FeedPosts
+      <Flex gap={20}>
+        <Box flex={2} py={10}>
+          <FeedPosts />
         </Box>
 
-        <Box>
+        <Box flex={3} mr={20} display={{base:"none", lg:"block"}} maxW={"300px"} >
           Suggested Users
         </Box>
       </Flex>
