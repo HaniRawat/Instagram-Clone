da9d4fcc1cd43421027b14d291a6f9e994eaba86
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-07-15 14:38:21 +0530

    env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,15 +4,18 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: "905026194567",
-  appId: "1:905026194567:web:04148a30d0d3e1c9b9fc22",
-  measurementId: "G-HCBHQFJP8Z"
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 
 
 const app = initializeApp(firebaseConfig);
