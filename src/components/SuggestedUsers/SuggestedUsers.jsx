a38a41fce9259dd143c1513d633e24dedec324c0
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-07-23 11:37:09 +0530

    fixed suggested user component

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,46 +1,40 @@
-import { Text, Flex, VStack, Box, Link } from '@chakra-ui/react'
-import SuggestedHeader from './SuggestedHeader'
-import SuggestedUser from './SuggestedUser'
-import useGetSuggestedUsers from '../../hooks/useGetSuggestedUsers'
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react";
+import SuggestedHeader from "./SuggestedHeader";
+import SuggestedUser from "./SuggestedUser";
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers";
 
+const SuggestedUsers = () => {
+	const { isLoading, suggestedUsers } = useGetSuggestedUsers();
 
-function SuggestedUsers() {
-  const {isLoading, suggestedUsers} = useGetSuggestedUsers()
+	if (isLoading) return null;
 
-  if(isLoading) return null
-  return (
-    <VStack py={8} px={6} gap={4}>
+	return (
+		<VStack py={8} px={6} gap={4}>
+			<SuggestedHeader />
 
-    <SuggestedHeader />
+			{suggestedUsers.length !== 0 && (
+				<Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+					<Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+						Suggested for you
+					</Text>
+					<Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+						See All
+					</Text>
+				</Flex>
+			)}
 
-    {suggestedUsers.length !== 0 && (
-      <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"} >
-
-    <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
-        Suggested for you
-    </Text>
-
-    <Text fontSize={12} fontWeight={"bold"} _hover={{color:"gray.400"}} cursor={"pointer"}>
-        See All
-    </Text>
-
-    </Flex>
-    )}
-
-    {suggestedUsers.map((user) => (
+			{suggestedUsers.map((user) => (
 				<SuggestedUser user={user} key={user.id} />
 			))}
 
-    <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
-    © 2024 Built By{" "}
-    <Link href='https://github.com/HaniRawat' target='_blank' color={"blue.500"} fontSize={14} >
+			<Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+				© 2024 Built By{" "}
+				<Link href='https://github.com/HaniRawat' target='_blank' color={"blue.500"} fontSize={14} >
         Hani Rawat
-    </Link>
-    </Box>
+        </Link>
+			</Box>
+		</VStack>
+	);
+};
 
-
-    </VStack>
-  )
-}
-
-export default SuggestedUsers
+export default SuggestedUsers;
